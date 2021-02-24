c01546be3a61d95740786cd8533dc816f4e98bb5
AlexFran1224 <alex_fran1224@hotmail.com>
2021-02-24 15:35:32 -0600

    Fin Seccion 3

---
+++ sockets/socket.js
@@ -0,0 +1,19 @@
+const {io} = require('../index');
+
+
+// Mensajes de Sockets
+io.on('connection', client => {
+    console.log('Cliente conectado');
+
+    client.on('disconnect', () => {
+        console.log('Cliente deconectado');
+    });
+
+    client.on('mensaje',(payload)=>{
+        console.log('Mensajeeeeeeee', payload);
+
+        io.emit('mensaje', {admin:'nuevo mensaje'});
+    });
+
+
+});
